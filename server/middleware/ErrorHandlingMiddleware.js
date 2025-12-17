319dd54fcda69f0f159c01d8cdb3c0c8f6fb76e6
SA0NT1 <86911179+Sopkafch@users.noreply.github.com>
2025-12-17 22:25:56 +0500

    Delete server directory

--- server/middleware/ErrorHandlingMiddleware.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (err, req, res, next) {
-    const status = err.status || 500
-    const message = err.message || 'Неизвестная ошибка'
-    return res.status(status).json({ message: message })
-}
