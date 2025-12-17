072ab2a3189d516d0ad24ecc81de1f15679cbe41
SA0NT1 <86911179+Sopkafch@users.noreply.github.com>
2025-12-17 22:29:43 +0500

    Add files via upload

---
+++ client/src/components/NevBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './navbar.css';
+
+import { logout } from '../api/router';
+
+function NavBar() {
+  const handleLogout = async() => {
+    try{
+      await logout()
+      window.location.href = '/login';
+    } catch(e){
+      console.log(e)
+    }
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="nav-left">📂 Файлы</div>
+
+      <button className="logout-button" onClick={handleLogout}>
+        Выйти
+      </button>
+    </nav>
+  );
+}
+
+export default NavBar;
